adf61ca3c82cb4366cc11ee6abc4c1f636c4de56
izhengfan <mrzhengfan@gmail.com>
2018-05-05 22:24:55 +0800

    add tags autosetup

--- autosetup.sh
+++ autosetup.sh
@@ -1,4 +1,18 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/.vimrc
+pip3 install --user pygments
+wget http://tamacom.com/global/global-6.6.2.tar.gz
+tar -xf global-6.6.2.tar.gz
+cd global-6.6.2/
+sudo apt install libncurses5-dev
+./configure
+make
+sudo make install
+mkdir -p ~/.vim/plugin
+cp /usr/local/share/gtags/gtags.vim ~/.vim/plugin/
+cp /usr/local/share/gtags/gtags-cscope.vim ~/.vim/plugin/
+cd ..
+wget https://raw.githubusercontent.com/skywind3000/vim/master/plugin/gutentags_plus.vim
+mv gutentags_plus.vim ~/.vim/plugin
 vim +PlugInstall +qall
