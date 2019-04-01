938029d4a6449024d2b69091bcb18bac45d7d7d4
izhengfan <hi@fzheng.me>
2019-04-01 11:35:31 +0800

    add nvim

--- nvim/autosetup.sh
+++ nvim/autosetup.sh
@@ -1,15 +1,14 @@
-sudo add-apt-repository ppa:jonathonf/vim
-sudo apt update
-sudo apt install vim vim-gtk3
-sudo apt install curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-{ echo "source "; pwd; echo "/.vimrc"; } | sed ':a;N;s/\n//;ba' >> ~/.vimrc
-pip3 install --user pygments
+{ echo "source "; pwd; echo "/../.vimrc"; } | sed ':a;N;s/\n//;ba' >> ~/.vimrc
+pip3 install --user pygments neovim
+pip2 install --user neovim
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim/init.vim
 wget http://tamacom.com/global/global-6.6.2.tar.gz
 tar -xf global-6.6.2.tar.gz
 cd global-6.6.2/
-sudo apt install libncurses5-dev
+sudo apt install libncurses5-dev xclip
 ./configure
 make
 sudo make install
@@ -19,7 +18,7 @@ cp /usr/local/share/gtags/gtags-cscope.vim ~/.vim/plugin/
 cd ..
 wget https://raw.githubusercontent.com/skywind3000/vim/master/plugin/gutentags_plus.vim
 mv gutentags_plus.vim ~/.vim/plugin
-vim +PlugInstall +qall
+nvim +PlugInstall +qall
 sudo apt install exuberant-ctags
 cd ~/.vim/plugged/YouCompleteMe
 ./install.py --clang-completer
