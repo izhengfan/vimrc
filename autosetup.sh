40b73836d402a72bbb7a102e87f2974f03e63054
izhengfan <hi@fzheng.me>
2019-04-01 14:59:36 +0800

    use universal-ctags

--- autosetup.sh
+++ autosetup.sh
@@ -1,11 +1,13 @@
 sudo add-apt-repository ppa:jonathonf/vim
 sudo apt update
 sudo apt install vim vim-gtk3
+
 sudo apt install curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 { echo "source "; pwd; echo "/.vimrc"; } | sed ':a;N;s/\n//;ba' >> ~/.vimrc
 pip3 install --user pygments
+
 wget http://tamacom.com/global/global-6.6.2.tar.gz
 tar -xf global-6.6.2.tar.gz
 cd global-6.6.2/
@@ -13,14 +15,24 @@ sudo apt install libncurses5-dev
 ./configure
 make
 sudo make install
+
 mkdir -p ~/.vim/plugin
 cp /usr/local/share/gtags/gtags.vim ~/.vim/plugin/
 cp /usr/local/share/gtags/gtags-cscope.vim ~/.vim/plugin/
 cd ..
 wget https://raw.githubusercontent.com/skywind3000/vim/master/plugin/gutentags_plus.vim
 mv gutentags_plus.vim ~/.vim/plugin
+
 vim +PlugInstall +qall
-sudo apt install exuberant-ctags
+
+git clone https://github.com/universal-ctags/ctags --depth=1
+cd ctags
+./autosetup.sh
+./configure
+make
+sudo make install
+cd ..
+
 cd ~/.vim/plugged/YouCompleteMe
 ./install.py --clang-completer
 cd ~/.vim/plugged/LeaderF
--- nvim/autosetup.sh
+++ nvim/autosetup.sh
@@ -1,10 +1,15 @@
+sudo apt update
+sudo apt install curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 { echo "source "; pwd; echo "/../.vimrc"; } | sed ':a;N;s/\n//;ba' >> ~/.vimrc
+
 pip3 install --user pygments neovim
 pip2 install --user neovim
+
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim/init.vim
+
 wget http://tamacom.com/global/global-6.6.2.tar.gz
 tar -xf global-6.6.2.tar.gz
 cd global-6.6.2/
@@ -12,14 +17,24 @@ sudo apt install libncurses5-dev xclip
 ./configure
 make
 sudo make install
+
 mkdir -p ~/.vim/plugin
 cp /usr/local/share/gtags/gtags.vim ~/.vim/plugin/
 cp /usr/local/share/gtags/gtags-cscope.vim ~/.vim/plugin/
 cd ..
 wget https://raw.githubusercontent.com/skywind3000/vim/master/plugin/gutentags_plus.vim
 mv gutentags_plus.vim ~/.vim/plugin
+
 nvim +PlugInstall +qall
-sudo apt install exuberant-ctags
+
+git clone https://github.com/universal-ctags/ctags --depth=1
+cd ctags
+./autosetup.sh
+./configure
+make
+sudo make install
+cd ..
+
 cd ~/.vim/plugged/YouCompleteMe
 ./install.py --clang-completer
 cd ~/.vim/plugged/LeaderF
