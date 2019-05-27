4b9305da9702c9d85f5295908e79a7185fe9c731
izhengfan <hi@fzheng.me>
2019-05-27 17:00:00 +0800

    divide postsetup

--- autosetup.sh
+++ autosetup.sh
@@ -35,15 +35,3 @@ cd ..
 
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
 sudo dpkg -i ripgrep_0.10.0_amd64.deb
-
-cd ~/.vim/plugged/YouCompleteMe
-./install.py --clang-completer
-cd ~/.vim/plugged/LeaderF
-./install.sh
-
-echo "let g:gruvbox_guisp_fallback = \"bg\"" >> ~/.vimrc
-echo "colorscheme gruvbox" >> ~/.vimrc
-echo "set clipboard^=unnamedplus" >> ~/.vimrc
-echo "\"let g:ycm_server_python_interpreter = '/usr/bin/python2'" >> ~/.vimrc
-cp .ycm_extra_conf.py ~/.ycm_extra_conf.py
-echo "let g:ycm_global_ycm_extra_conf='~/.ycm_extra_conf.py'" >> ~/.vimrc
